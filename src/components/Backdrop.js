8e4e8af097ecc9d0859bde79fa116bbd4ebd29f3
AyjayJr <ayjayjr@hotmail.com>
2022-12-22 16:01:04 -0500

    use state values to dynamically render a modal

---
+++ src/components/Backdrop.js
@@ -0,0 +1,8 @@
+function Backdrop() {
+	// in react any component can be self closing if it doesn't contain anything.
+	return (
+		<div className="backdrop"/>
+	);
+}
+
+export default Backdrop;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,32 @@
+// react hook that creates a state that react is aware of
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
 // Todo component that can be written once and reused mutiple times
 function Todo(props) {
+  // useState always returns an array of a state value and a function to set the value
+  // use array destructuring to capture the values that useState returns
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  // nested function for deleting
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }  
+
+
   return (
     // className is one of the few times a JS property doesn't match an HTML attribute
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
+      {/* use the state value to dynamcally render certain components */}
+      { modalIsOpen && <Modal/> }
+      { modalIsOpen && <Backdrop/> }
     </div>
-  )
+  );
 }
 
 export default Todo;
